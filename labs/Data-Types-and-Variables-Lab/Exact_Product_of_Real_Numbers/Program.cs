c7a90788d6a45774dfceba0100d3139b8af67168
userv <rosen.t.angelov@gmail.com>
2017-01-30 14:24:24 +0200

    Lab1 Solution

---
+++ labs/Data-Types-and-Variables-Lab/Exact_Product_of_Real_Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exact_Product_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal product=1;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                decimal number=decimal.Parse(Console.ReadLine());
+                product *= number;
+            }
+            Console.WriteLine(product);
+        }
+    }
+}
---
+++ labs/Data-Types-and-Variables-Lab/Transport/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 4 + 8 + 12;
+            int n = int.Parse(Console.ReadLine());
+            int courses = (int) Math.Ceiling((double) n/capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
