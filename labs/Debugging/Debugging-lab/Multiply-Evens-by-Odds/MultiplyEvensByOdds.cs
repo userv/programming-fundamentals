6e409c2eba826fa85810e97d84a96109bdd8cd7b
userv <rosen.t.angelov@gmail.com>
2017-02-10 14:58:55 +0200

    Debugging labs

---
+++ labs/Debugging/Debugging-lab/Multiply-Evens-by-Odds/MultiplyEvensByOdds.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiply_Evens_by_Odds
+{
+    class MultiplyEvensByOdds
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            var evenOddProduct = GetMultipleOfEvensAndOdds(num);
+            Console.WriteLine(evenOddProduct);
+        }
+
+        static int GetMultipleOfEvensAndOdds(int num)
+        {
+            var product = GetSumOfOddDigits(num)*GetSumOfEvenDigits(num);
+            return product;
+        }
+
+        private static int GetSumOfOddDigits(int num)
+        {
+            var sum = 0;
+            num = Math.Abs(num);
+            while (num > 0)
+            {
+                var currentDigit = num%10;
+                if (currentDigit%2 != 0)
+                    sum += currentDigit;
+
+                num /= 10;
+            }
+
+            return sum;
+        }
+
+        static int GetSumOfEvenDigits(int num)
+        {
+            var sum = 0;
+            num = Math.Abs(num);
+            while (num > 0)
+            {
+                var currentDigit = num%10;
+                if (currentDigit%2 == 0)
+                    sum += currentDigit;
+
+                num /= 10;
+            }
+
+            return sum;
+        }
+
+
+    }
+}
---
+++ labs/Debugging/Debugging-lab/Price-Change-Alert/PriceChangeAlert.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class PriceChangeAlert
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double threshold = double.Parse(Console.ReadLine());
+        double price = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            double currentPrice = double.Parse(Console.ReadLine());
+            double difference = Proc(price, currentPrice);
+            bool isSignificantDifference = isDifference(threshold, difference);
+            string message = GetMessage(currentPrice, price, difference, isSignificantDifference);
+            Console.WriteLine(message);
+            price = currentPrice;
+        }
+    }
+
+    private static string GetMessage(double currentPrice, double price, double difference, bool isSignificantDifference)
+    {
+        string message = "";
+        if (difference == 0)
+        {
+            message = string.Format("NO CHANGE: {0}", price);
+        }
+        else if (!isSignificantDifference)
+        {
+            message = string.Format("MINOR CHANGE: {0} to {1} ({0:P})", price, currentPrice, difference);
+        }
+        else if (isSignificantDifference && (difference > 0))
+        {
+            message = string.Format("PRICE UP: {0} to {1} ({0:P})", price, currentPrice, difference);
+        }
+        else if (isSignificantDifference && (difference < 0))
+        {
+            message = string.Format("PRICE DOWN: {0} to {1} ({0:P})", price, currentPrice, difference);
+        }
+        return message;
+    }
+
+    private static bool isDifference(double threshold, double difference)
+    {
+        if (Math.Abs(difference) > threshold)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static double Proc(double price, double currentPrice)
+    {
+        double result = (currentPrice - price) / price;
+        return result;
+    }
+}
