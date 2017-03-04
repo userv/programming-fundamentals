ace7c06f78a5bc9b68bb53f3318a372112f48f97
userv <rosen.t.angelov@gmail.com>
2017-03-04 16:49:22 +0200

    Japanese-Roulette/JapaneseRoulette.cs

--- exercises/Array-and-List-Algorithms-More-Exercises/Japanese-Roulette/JapaneseRoulette.cs
+++ exercises/Array-and-List-Algorithms-More-Exercises/Japanese-Roulette/JapaneseRoulette.cs
@@ -12,14 +12,65 @@ namespace Japanese_Roulette
         {
             byte[] cylinder = Console.ReadLine().Split(' ').Select(byte.Parse).ToArray();
             List<string> playersList = Console.ReadLine().Split(' ').ToList();
-            int currentPos = 0;
+            int currentPos = 2;
+            bool isEverybodyGotLucky = true;
             for (int i = 0; i < playersList.Count; i++)
             {
                 string[] playerParams = playersList[i].Split(',').ToArray();
-                string direction = playerParams[0];
-                int strenght = int.Parse(playerParams[1]);
+                string direction = playerParams[1].ToLower();
+                int strength = int.Parse(playerParams[0]);
+                if (direction == "right")
+                {
 
+                    
+                    while (strength > 0)
+                    {
+                        currentPos--;
+                        if (currentPos < 0)
+                        {
+                            currentPos = cylinder.Length - 1;
+                        }
+                        strength--;
+                    }
+                    if (cylinder[currentPos] == 1)
+                    {
+                        Console.WriteLine($"Game over! Player {i} is dead.");
+                        isEverybodyGotLucky = false;
+                        break;
+                    }
+                  
+                }
+                if (direction == "left")
+                {
+                    
+                    while (strength > 0)
+                    {
+                        currentPos++;
+                        if (currentPos > cylinder.Length - 1)
+                        {
+                            currentPos = 0;
+                        }
+                        strength--;
 
+                    }
+                    if (cylinder[currentPos] == 1)
+                    {
+                        Console.WriteLine($"Game over! Player {i} is dead.");
+                        isEverybodyGotLucky = false;
+                        break;
+                    }
+                   
+                }
+                currentPos--;
+                if (currentPos<0)
+                {
+                    currentPos = cylinder.Length - 1;
+                }
+
+            }
+            if (isEverybodyGotLucky)
+            {
+                Console.WriteLine("Everybody got lucky!");
             }
 
         }
