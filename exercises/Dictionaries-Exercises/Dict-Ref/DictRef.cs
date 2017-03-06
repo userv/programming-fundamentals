0aa84221b67e072fc58dc276430e86f3c0c82aba
userv <rosen.t.angelov@gmail.com>
2017-03-06 16:06:17 +0200

    Dictionaries Exercises

---
+++ exercises/Dictionaries-Exercises/Dict-Ref/DictRef.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dict_Ref
+{
+    public class DictRef
+    {
+        public static void Main()
+        {
+            Dictionary<string, int> refDictionary = new Dictionary<string, int>();
+            string inputLine = string.Empty;
+            while (inputLine != "end")
+            {
+                inputLine = Console.ReadLine();
+                if (inputLine == "end") continue;
+                string[] entry = inputLine
+                    .Split(new char[] { '=' }, StringSplitOptions.None)
+                    .ToArray();
+                entry[0] = entry[0].Trim();
+                entry[1] = entry[1].Trim();
+
+                int value;
+                bool success = int.TryParse(entry[1], out value);
+                if (success)
+                {
+                    if (refDictionary.ContainsKey(entry[0]))
+                    {
+                        refDictionary[entry[0]] = value;
+                    }
+                    else
+                    {
+                        refDictionary.Add(entry[0], value);
+                    }
+
+                }
+                else
+                {
+                    if (refDictionary.ContainsKey(entry[0]) && refDictionary.ContainsKey(entry[1]))
+                    {
+                        refDictionary[entry[0]] = refDictionary[entry[1]];
+                    }
+                    else if (!refDictionary.ContainsKey(entry[0]) && refDictionary.ContainsKey(entry[1]))
+                    {
+                        refDictionary.Add(entry[0], refDictionary[entry[1]]);
+                    }
+                }
+
+            }
+            foreach (var kvp in refDictionary)
+            {
+                Console.WriteLine($"{kvp.Key} === {kvp.Value}");
+
+            }
+        }
+    }
+}
---
+++ exercises/Dictionaries-Exercises/Letter=Repetition/LetterRepetition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Letter_Repetition
+{
+    public class LetterRepetition
+    {
+        public static void Main()
+        {
+            string str = Console.ReadLine();
+            Dictionary<char,int> letters=new Dictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!letters.ContainsKey(str[i]))
+                {
+                    letters[str[i]] = 0;
+                }
+                letters[str[i]]++;
+            }
+            foreach (var kvp in letters)
+            {
+                var letter = kvp.Key;
+                var occurence = kvp.Value;
+                Console.WriteLine($"{letter} -> {occurence}");
+
+            }
+        }
+    }
+}
