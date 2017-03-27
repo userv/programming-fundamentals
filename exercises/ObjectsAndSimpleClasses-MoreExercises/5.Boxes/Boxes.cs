380c08f22c55d6a29ec670a818e966684738c184
userv <rosen.t.angelov@gmail.com>
2017-03-27 16:22:17 +0300

    Added Box and Messages  solutions

--- exercises/ObjectsAndSimpleClasses-MoreExercises/5.Boxes/Boxes.cs
+++ exercises/ObjectsAndSimpleClasses-MoreExercises/5.Boxes/Boxes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,115 @@ namespace _5.Boxes
 {
     public class Boxes
     {
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public static double CalculateDistance(Point p1, Point p2)
+            {
+                int dX = p1.X - p2.X;
+                int dY = p1.Y - p2.Y;
+                double distance = Math.Sqrt(dX * dX + dY * dY);
+                return distance;
+            }
+        }
+        static Point ReadPoint(string inputString, char splitChar)
+        {
+            int[] pointCoordinates = inputString.Split(splitChar).Select(int.Parse).ToArray();
+            Point p = new Point();
+            p.X = pointCoordinates[0];
+            p.Y = pointCoordinates[1];
+            return p;
+        }
+        static Point ReadPoint()
+        {
+            int[] pointCoordinates = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Point p = new Point();
+            p.X = pointCoordinates[0];
+            p.Y = pointCoordinates[1];
+            return p;
+        }
+
+        public class Box
+        {
+            public Point UpperLeft { get; set; }
+            public Point UpperRight { get; set; }
+            public Point BottomLeft { get; set; }
+            public Point BottomRight { get; set; }
+
+            public int Width
+            {
+                get { return (int)Point.CalculateDistance(UpperLeft, UpperRight); }
+            }
+
+            public int Height
+            {
+                get { return (int)Point.CalculateDistance(UpperLeft, BottomLeft); }
+            }
+
+            public int CalculatePerimeter()
+            {
+                return (2 * Width + 2 * Height);
+            }
+
+            public int CalculateArea()
+            {
+                return (Width * Height);
+
+            }
+            public static int CalculatePerimeter(int width, int height)
+            {
+                return (2 * width + 2 * height);
+            }
+
+            public static int CalculateArea(int width, int height)
+            {
+                return (width * height);
+
+            }
+        }
+
+        public static Box ReadBox(string inputData)
+        {
+
+            List<Point> points = new List<Point>();
+            string[] pointStrings = inputData.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            foreach (var point in pointStrings)
+            {
+                // Point p = ReadPoint(point,':');
+                points.Add(ReadPoint(point, ':'));
+            }
+            Box newBox = new Box
+            {
+                UpperLeft = points[0],
+                UpperRight = points[1],
+                BottomLeft = points[2],
+                BottomRight = points[3]
+            };
+
+            return newBox;
+        }
+
+
+
         public static void Main()
         {
+            List<Box> boxes = new List<Box>();
+            string inputData = Console.ReadLine();
+            while (inputData != "end")
+            {
+                Box newBox = ReadBox(inputData);
+                boxes.Add(newBox);
+                inputData = Console.ReadLine();
+            }
+            foreach (var box in boxes)
+            {
+                Console.WriteLine($"Box: {box.Width}, {box.Height}");
+                Console.WriteLine($"Perimeter: {box.CalculatePerimeter()}");
+                Console.WriteLine($"Area: {box.CalculateArea()}");
+            }
 
         }
     }
---
+++ exercises/ObjectsAndSimpleClasses-MoreExercises/6.Messages/Messages.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Messages
+{
+    public class Messages
+    {
+        public class User
+        {
+            public string Username { get; set; }
+            public List<Message> ReceivedMessages = new List<Message>();
+
+        }
+        public class Message
+        {
+            public string Content { get; set; }
+            public User Sender { get; set; }
+        }
+
+        public static void SendMessage(List<User> registredUsers, string senderUsername, string recipientUsername, string content)
+        {
+            Message newMessage = new Message()
+            {
+                Content = content,
+                Sender = new User { Username = senderUsername }
+            };
+
+            var user = registredUsers.Where(r => r.Username == recipientUsername)
+                .FirstOrDefault();
+            if (user != null)
+            {
+                registredUsers.Where(r => r.Username == recipientUsername)
+                    .FirstOrDefault()
+                    .ReceivedMessages.Add(newMessage);
+            }
+        }
+        public static void Main()
+        {
+            List<User> registredUsers = new List<User>();
+            string inputLine = Console.ReadLine();
+            while (inputLine != "exit")
+            {
+                string[] tokens = inputLine.Split(' ').ToArray();
+                //   string command = tokens[0];
+                if (tokens.Length < 4)
+                {
+                    if (tokens[0] == "register")
+                    {
+                        registredUsers.Add(new User() { Username = tokens[1] });
+                    }
+                }
+                else if (tokens.Length == 4)
+                {
+                    string senderUsername = tokens[0];
+                    string recipientUsername = tokens[2];
+                    string content = tokens[3];
+                    SendMessage(registredUsers, senderUsername, recipientUsername, content);
+                }
+                inputLine = Console.ReadLine();
+            }
+            string[] users = Console.ReadLine().Split(' ').ToArray();
+            var user1Msg =
+                registredUsers.Where(username => username.Username == users[0]).FirstOrDefault();
+                    
+            var user2Msg = registredUsers.Where(username => username.Username == users[1]).FirstOrDefault();
+            
+            var msg1 = user1Msg.ReceivedMessages.Where(sender => sender.Sender.Username == users[1]).ToArray();
+            var msg2 = user2Msg.ReceivedMessages.Where(sender => sender.Sender.Username == users[0]).ToArray();
+            if (msg1.Length < 1 && msg2.Length<1)
+            {
+                Console.WriteLine("No messages");
+                return;
+            }
+            int numberOfMessages=0;
+           if (msg1.Length> msg2.Length)
+            {
+                numberOfMessages = msg1.Length;
+            }
+            else
+            {
+                numberOfMessages = msg2.Length;
+            }
+            
+            for (int i = 0; i <numberOfMessages; i++)
+            {
+                if (msg2.Length > i)
+                {
+                    Console.WriteLine($"{user1Msg.Username}: {msg2[i].Content}");
+                   
+                }
+                if (msg1.Length > i)
+                {
+                    Console.WriteLine($"{msg1[i].Content} :{user2Msg.Username}");
+                  
+                }
+                
+               
+            }
+                
+        }
+    }
+}
