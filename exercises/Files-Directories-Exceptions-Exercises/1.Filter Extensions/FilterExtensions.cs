82d0c192720ae187111d19f9a76030d85af22d46
userv <rosen.t.angelov@gmail.com>
2017-03-29 19:37:53 +0300

    Files, Directories and Exceptions - Exercises

---
+++ exercises/Files-Directories-Exceptions-Exercises/1.Filter Extensions/FilterExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Filter_Extensions
+{
+    public class FilterExtensions
+    {
+        public static void Main(string[] args)
+        {
+            string filterExtension = Console.ReadLine();
+            filterExtension = "." + filterExtension;
+            try
+            {
+                string[] files = Directory.GetFiles("input");
+                foreach (var file in files)
+                {
+                    FileInfo fileInfo=new FileInfo(file);
+                    if (fileInfo.Extension==filterExtension)
+                    {
+                        Console.WriteLine(fileInfo.Name);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Path not found!");
+                //Console.WriteLine(e);
+               // throw;
+            }
+            
+
+        }
+    }
+}
---
+++ exercises/Files-Directories-Exceptions-Exercises/2.HTML Contents/HTMLContents.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _2.HTML_Contents
+{
+    public class HTMLContents
+    {
+        public static void Main()
+        {
+            Dictionary<string, string> contentDict = new Dictionary<string, string>();
+            string inputLine = Console.ReadLine();
+            while (inputLine != "exit")
+            {
+                string[] inputParams = inputLine.Split(' ').ToArray();
+                if (inputParams.Length < 2) continue;
+                string tag = inputParams[0];
+                string content = inputParams[1];
+                if (!contentDict.ContainsKey(tag))
+                {
+                    contentDict.Add(tag, content);
+                }
+                contentDict[tag] = content;
+                inputLine = Console.ReadLine();
+            }
+            GenerateHtmlFile(contentDict);
+        }
+
+        public static void GenerateHtmlFile(Dictionary<string, string> contentDict)
+        {
+            string fileName = "index.html";
+            List<string> contentList=new List<string>();
+            contentList.Add($"<!DOCTYPE html>");
+            contentList.Add($"<html>");
+            contentList.Add($"<head>");
+            contentList.Add($"\t<title>{contentDict["title"]}</title>");
+            contentList.Add($"</head>");
+            contentList.Add($"<body>");
+            foreach (var line in contentDict)
+            {
+                if (line.Key!="title")
+                {
+                    contentList.Add($"\t<{line.Key}>{line.Value}</{line.Key}>");
+                }
+                
+            }
+            contentList.Add($"</body>");
+            contentList.Add($"</html>");
+            File.WriteAllLines(fileName,contentList);
+        }
+    }
+}
