28774cc7153f0dce1318256f8a6611fcd20f2767
userv <rosen.t.angelov@gmail.com>
2017-04-13 10:54:59 +0300

    Regular Expressions (RegEx) -Lab

---
+++ labs/Regular Expressions (RegEx) -Lab/1.Replace-a-tag/ReplaceTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _1.Replace_a_tag
+{
+    public class ReplaceTag
+    {
+        public static void Main()
+        {
+            string text = "<ul> <li> <a href=\"http://softuni.bg\">SoftUni</a></ li > </ ul >";
+            string pattern = @"<a.*?href.*?=(.*)>(.*)<\/a>";
+            string replace = @"[URL href=$1>$2[/URL]";
+            Regex regex=new Regex(pattern);
+            bool match = regex.IsMatch(text);
+             var maches = regex.Matches(text);
+            
+            string replaced = Regex.Replace(text, pattern, replace);
+            Console.WriteLine(replaced);
+
+        }
+    }
+}
