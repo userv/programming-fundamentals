bcd5430dbea0b4020e31b87074fcd52cfb64eaf5
userv <rosen.t.angelov@gmail.com>
2017-04-14 20:48:04 +0300

    Exam-Preparation II

---
+++ exercises/Exam-Preparation II/1.Charity Marathon/CharityMarathon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Charity_Marathon
+{
+    class CharityMarathon
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int runnersCount = int.Parse(Console.ReadLine());
+            int averageLaps = int.Parse(Console.ReadLine());
+            int lapLength = int.Parse(Console.ReadLine());
+            int trackCapacity = int.Parse(Console.ReadLine());
+            double moneyPerKilometer = double.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
