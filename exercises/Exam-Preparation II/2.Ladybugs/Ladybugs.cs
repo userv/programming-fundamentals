54de170144947ba5fd4a9fed8450a1c04e25d651
userv <rosen.t.angelov@gmail.com>
2017-04-15 16:04:03 +0300

    Ladybugs

---
+++ exercises/Exam-Preparation II/2.Ladybugs/Ladybugs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Ladybugs
+{
+    public class Ladybugs
+    {
+        public static void Main()
+        {
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MinValue);
+
+
+
+
+        }
+    }
+}
