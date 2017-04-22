dc5798d259d75de61dd00cec6eba1f37e4199008
userv <rosen.t.angelov@gmail.com>
2017-04-22 14:52:59 +0300

    RageQuit and Files solutions added

---
+++ exercises/Exam-Preparation III/3.Rage Quit/RageQuit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace _3.Rage_Quit
+{
+    public class RageQuit
+    {
+        public static void Main()
+        {
+            string pattern = @"(\D+)(\d+)";
+            string inputLine = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(inputLine);
+            StringBuilder msg = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                string str = match.Groups[1].Value;
+                int count = int.Parse(match.Groups[2].Value);
+                msg.Append(RepeatString(str, count).ToUpper());
+            }
+            string result = msg.ToString();
+            Console.WriteLine($"Unique symbols used: {result.Distinct().Count()}");
+            Console.WriteLine(result);
+        }
+
+        private static string RepeatString(string str, int count)
+        {
+            StringBuilder repeatedString = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                repeatedString.Append(str);
+            }
+            return repeatedString.ToString();
+        }
+    }
+}
---
+++ exercises/Exam-Preparation III/4.Files/Files.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.Files
+{
+    class Files
+    {
+        static void Main(string[] args)
+        {
+            //      ROOTFOLDER,         FileName, Size
+            Dictionary<string, Dictionary<string, long>> rootFolder = new Dictionary<string, Dictionary<string, long>>();
+            //        FileName,Extension
+            Dictionary<string, string> fileWithExtension = new Dictionary<string, string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputLine = Console.ReadLine().Split('\\');
+                string root = inputLine[0];
+                string[] fileParams = inputLine.Last().Split(';');
+                string filename = fileParams[0].Trim();
+                string fileExtension = filename.Split('.').Last();
+                long fileSize = long.Parse(fileParams[1]);
+                if (!rootFolder.ContainsKey(root))
+                {
+                    rootFolder[root] = new Dictionary<string, long>();
+                }
+                if (!fileWithExtension.ContainsKey(filename))
+                {
+                    fileWithExtension.Add(filename, fileExtension);
+                }
+                fileWithExtension[filename] = fileExtension;
+                rootFolder[root][filename] = fileSize;
+            }
+            string[] inputQuery =
+                Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string queryRoot = inputQuery[2];
+            string queryExtension = inputQuery[0];
+            Dictionary<string, long> extractedFiles = new Dictionary<string, long>();
+            if (rootFolder.ContainsKey(queryRoot))
+            {
+                foreach (var file in rootFolder[queryRoot]) 
+                {
+                    if (fileWithExtension[file.Key] == queryExtension)
+                    {
+                        extractedFiles.Add(file.Key, file.Value);
+                    }
+                }
+            }
+            if (extractedFiles.Count > 0)
+            {
+                foreach (var file in extractedFiles.OrderByDescending(s => s.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{file.Key} - {file.Value} KB");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+}
