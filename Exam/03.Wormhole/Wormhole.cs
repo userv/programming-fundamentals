2d93637ae34a65bc0b68ca82b7ffc32ab09b7c90
userv <rosen.t.angelov@gmail.com>
2017-05-27 15:20:04 +0300

    Exams

---
+++ Exam/03.Wormhole/Wormhole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Wormhole
+{
+    public class Wormhole
+    {
+        public static void Main()
+        {
+            int[] wormholes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int steps = 0;
+            int currentIndex = 0;
+            int lastIndex = wormholes.Length - 1;
+            while (currentIndex <= lastIndex)
+            {
+                if (wormholes[currentIndex] == 0)
+                {
+                    currentIndex++;
+                    steps++;
+                }
+                else
+                {
+                    int tmp = wormholes[currentIndex];
+                    wormholes[currentIndex] = 0;
+                    currentIndex = tmp;
+                }
+            }
+            Console.WriteLine(steps);
+        }
+    }
+}
